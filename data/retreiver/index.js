a939f1baa70fe2de8dcb5243c5f130f0110f3da6
Dardaul Hoque <rudro1152@gmail.com>
2023-10-16 17:23:00 +0600

    housekeeping

--- data/retreiver/index.js
+++ data/retreiver/index.js
@@ -1,26 +1,20 @@
-import { retreiveDailyPowerStats } from "./power.js"
-import { retreiveDailyRainfall } from "./rain.js"
-import { namesToAreaCodes } from "./power.js"
+import { guardAgainstInvalidInputs } from "./utils.js"
+import {
+  retreiveDailyPowerStats,
+  retreiveDailyWeather,
+} from "./weather-power.js"
+
 const [_, __, area, fromDate, toDate] = process.argv
+guardAgainstInvalidInputs(area, fromDate, toDate)
 
-if (area === undefined || fromDate === undefined || toDate === undefined) {
-  console.error("Usage: node index.js <area> <fromDate> <toDate>")
-  console.log("Example: node index.js dhaka 01/01/2013 03/01/2013")
-  process.exit(1)
-}
-
-if (namesToAreaCodes[area] === undefined) {
-  console.error("Invalid area name")
-  process.exit(1)
-}
-
-// const elements = await retreiveDailyPowerStats(area, fromDate, toDate)
-const daily = await retreiveDailyRainfall(area, fromDate, toDate)
-
-const combineRainfallWithPower = async (area, fromDate, toDate) => {
+/**
+ * @param {string} fromDate is in the format DD/MM/YYYY.
+ * @param {string} toDate is in the format DD/MM/YYYY.
+ */
+const combineWeatherWithPower = async (area, fromDate, toDate) => {
   const [powerStats, rainfallStats] = await Promise.all([
     retreiveDailyPowerStats(area, fromDate, toDate),
-    retreiveDailyRainfall(area, fromDate, toDate),
+    retreiveDailyWeather(area, fromDate, toDate),
   ])
 }
 
--- data/retreiver/utils.js
+++ data/retreiver/utils.js
@@ -1,4 +1,23 @@
+import { namesToAreaCodes } from "./weather-power.js"
+
 export const reformatDate = (date) => {
   const [day, month, year] = date.split("/")
   return `${year}-${month}-${day}`
+}
+
+export const guardAgainstInvalidInputs = (area, fromDate, toDate) => {
+  if (
+    area === undefined ||
+    fromDate === undefined ||
+    toDate === undefined
+  ) {
+    console.error("Usage: node index.js <area> <fromDate> <toDate>")
+    console.log("Example: node index.js dhaka 01/01/2013 03/01/2013")
+    process.exit(1)
+  }
+
+  if (namesToAreaCodes[area] === undefined) {
+    console.error("Invalid area name")
+    process.exit(1)
+  }
 }
