14574d589b8dc44e0add6cf2e932f80cc90b209d
Dardaul Hoque <rudro1152@gmail.com>
2023-10-18 00:54:56 +0600

    drop rows with missing power records

--- data/retreiver/fetch.js
+++ data/retreiver/fetch.js
@@ -11,30 +11,25 @@ const generateCombinedCSV = async (area, fromDate, toDate) => {
     retreiveDailyWeather(area, fromDate, toDate),
   ])
 
-  const len = weatherStats.time.length
+  const len = powerStats.length
   const combinedRecords = new Array(len)
 
-  for (let i = 0, j = 0, demand, loadShed; i < len; i++) {
+  for (let i = 0, j = 0; j < len; i++) {
     // Handling missing records in Power dataset
     if (powerStats[j].date === weatherStats.time[i]) {
-      demand = powerStats[j].demand
-      loadShed = powerStats[j].loadShed
+      combinedRecords[j] = {
+        date: weatherStats.time[i],
+        demand: powerStats[j].demand,
+        loadShed: powerStats[j].loadShed,
+        rainfall: weatherStats.rain_sum[i],
+        mean_temperature: weatherStats.temperature_2m_mean[i],
+        relativehumidity_2m: weatherStats.relativehumidity_2m[i],
+        windspeed_100m: weatherStats.windspeed_100m[i],
+        daytime_length: weatherStats.daytime_length[i],
+      }
       j++
     } else {
       console.log("Missing power stats for: ", weatherStats.time[i])
-      demand = ""
-      loadShed = ""
-    }
-
-    combinedRecords[i] = {
-      date: weatherStats.time[i],
-      demand,
-      loadShed,
-      rainfall: weatherStats.rain_sum[i],
-      mean_temperature: weatherStats.temperature_2m_mean[i],
-      relativehumidity_2m: weatherStats.relativehumidity_2m[i],
-      windspeed_100m: weatherStats.windspeed_100m[i],
-      daytime_length: weatherStats.daytime_length[i],
     }
   }
 
